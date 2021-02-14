7a2f5273ba0183e80a4c0b7b87df3e964495f7b5
Seojunhwan <seojunhwan@kakao.com>
2021-02-15 02:01:42 +0900

    create init, router js

--- app.js
+++ app.js
@@ -4,20 +4,11 @@ import express from "express";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyparser from "body-parser";
-
 const app = express();
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhos:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from ass");
 
 const handleProfile = (req, res) => res.send("Your are on my profile");
-
-const middleware = (req, res, next) => {
-  res.send("not happening");
-};
 
 app.use(cookieParser());
 app.use(bodyparser.json());
@@ -25,8 +16,8 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(helmet());
 
-app.get("/", middleware, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
